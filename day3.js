731bd23d5027fd1d0b6aa7bdbfdb887589431446
Tahirah Roohi <157677736+TahirahWebDev@users.noreply.github.com>
2024-06-22 23:21:11 -0700

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,14 @@
+"use strict";
+//  Question 7
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
+//  Question 8
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//  Question 9
+let favNum = 4;
+console.log(`My favourite number is ${favNum}`);
---
+++ day3.ts
@@ -0,0 +1,20 @@
+//  Question 7
+
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
+
+
+//  Question 8
+
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+
+
+//  Question 9
+
+let favNum:number = 4;
+console.log(`My favourite number is ${favNum}`)
